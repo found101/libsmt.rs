ca43a2f759ded5ae85eec45513fe5cf47ca28098
Sushant <sushant.dinesh94@gmail.com>
2016-03-02 14:48:29 +0530

    Temporarily rollback array_ex modiifications

--- src/theories/array_ex.rs
+++ src/theories/array_ex.rs
@@ -8,6 +8,8 @@ use smt::SMTNode;
 pub enum OpCodes {
     Select,
     Store,
+    FreeVar(String),
+    Const(usize),
 }
 
 #[derive(Clone, Debug)]
@@ -26,20 +28,19 @@ where X: fmt::Debug + fmt::Display + Clone,
     Const(Box<X>, Box<Y>),
 }
 
-impl<X, Y> fmt::Display for OpCodes<X, Y>
-where X: fmt::Display + fmt::Debug + Clone,
-      Y: fmt::Display + fmt::Debug + Clone {
+impl fmt::Display for OpCodes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let s = match *self {
             OpCodes::Select => "select".to_owned(),
             OpCodes::Store => "store".to_owned(),
             OpCodes::FreeVar(ref s) => s.clone(),
+            _ => unimplemented!(),
         };
         write!(f, "{}", s)
     }
 }
 
-impl_smt_node!(OpCodes<X, Y>, define consts [OpCodes::Const(_)]);
+impl_smt_node!(OpCodes, define consts [OpCodes::Const(_)]);
 
 #[derive(Clone, Debug)]
 pub enum Sorts<X, Y>
